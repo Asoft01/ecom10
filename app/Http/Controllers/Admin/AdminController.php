ae8a098344b487fed436671b6a5bc60ed5e2e36e
Adeleke Hammed Lekan <lekhad19@gmail.com>
2023-07-20 15:52:51 +0100

    Admin Logout

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -24,4 +24,9 @@ class AdminController extends Controller
         }
         return view('admin.login');
     }
+
+    public function logout(){
+        Auth::guard('admin')->logout(); 
+        return redirect('admin/login');
+    }
 }
